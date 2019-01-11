0a32dd56eeba033ba5ecbe6eabc2237731f6997d
Tej Poudel <newsbinodp@gmail.com>
2019-01-10 18:04:18 -0800

    Modiedfied Readmes

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 var express = require("express");
 var app = express();
+
+
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, function() {
+   console.log("Localhost is listening on " + PORT); 
+});
