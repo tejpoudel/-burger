4d4c1b07d631ba6d018841ec656b985ee35374c0
Tej Poudel <newsbinodp@gmail.com>
2019-02-09 12:09:29 -0800

    Edited Readme's

--- config/connection.js
+++ config/connection.js
@@ -17,14 +17,6 @@ if (process.env.JAWSDB_URL) {
 });
 }
 
-// connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "developer1", 
-//     database: "burgers_db"
-// });
-
 connection.connect(function(err) {
     if(err) {
         console.error("error connecting: " + err.stack);
