58f63b465cf01dbf8e5a7daef326a815e63b3c38
Tej Poudel <newsbinodp@gmail.com>
2019-02-14 20:13:18 -0800

    updating

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../Models/burger.js");
+var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
     burger.all(function(data) {
--- models/burger.js
+++ models/burger.js
@@ -3,7 +3,7 @@
 
 
 //Import the ORM to create functions that will interact with the database
-var orm = require("../Config/orm.js");
+var orm = require("../config/orm.js");
 
 //Code that will call the ORM functions using burger specific input for the ORM.
 
